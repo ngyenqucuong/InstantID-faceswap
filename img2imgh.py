8c514f5e0e7bdcf2686bdfba081ff390ac6ad08e
Nguyễn Quốc Cường <104240677@student.vgu.edu.vn>
2025-08-05 10:38:53 +0700

    edit port

--- img2imgh.py
+++ img2imgh.py
@@ -406,4 +406,4 @@ if __name__ == "__main__":
     os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
     os.environ["TORCH_USE_CUDA_DSA"] = "1"
     
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8888)
